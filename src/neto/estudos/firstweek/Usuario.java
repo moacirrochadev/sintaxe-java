38120126347b2409d6ac8e9f3e2ad16a961f42c4
moacirrochadev <empreendedormoacirneto@gmail.com>
2024-07-03 11:51:13 -0300

    Criarndo as classes da SmartTV

--- src/neto/estudos/firstweek/Usuario.java
+++ src/neto/estudos/firstweek/Usuario.java
@@ -4,6 +4,13 @@ public class Usuario {
     public static void main(String[] args) {
         SmartTV smartTv = new SmartTV();
 
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(13);
+
         System.out.println("TV Ligada: " + smartTv.ligada);
         System.out.println("Canal atual: " + smartTv.canal);
         System.out.println("Volume atual: " + smartTv.volume);
